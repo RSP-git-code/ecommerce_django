48ed4767d68ecf438eac3dd15d60786c7fc016e3
Riya Sangram Pawar <pawarriya9999@gmail.com>
2025-07-11 13:40:24 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -130,6 +130,7 @@ STATIC_URL = '/static/'
 import os
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+     os.path.join(BASE_DIR, 'item_images'),
 ]
 
 # Default primary key field type
