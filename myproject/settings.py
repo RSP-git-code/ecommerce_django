c8b8098553f417516f0b8101eebb7caf82630924
Riya Sangram Pawar <pawarriya9999@gmail.com>
2025-07-14 20:59:43 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -133,6 +133,7 @@ STATICFILES_DIRS = [
      os.path.join(BASE_DIR, 'item_images'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+CSRF_TRUSTED_ORIGINS = ["https://proventuremarketplace.onrender.com"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
